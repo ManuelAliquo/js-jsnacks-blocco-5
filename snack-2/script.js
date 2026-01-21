a32721ac8458e9fce46e70e98d6422e52dc36bcb
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-21 14:30:56 +0100

    snack 1-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,19 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
+
+people.forEach((person) => console.log(person.name));
+
+//##########
+console.log("---------");
+//##########
+
+const peopleNames = people.map((person) => person.name);
+
+console.log(peopleNames);
+
 // Risultato: 'Paolo', 'Giulia', 'Marco'
